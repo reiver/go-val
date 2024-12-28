636647010a56fab829aeb85ab4a451b5f7986c97
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-28 13:57:33 -0800

    initial code

--- errors.go
+++
@@ -1,9 +0,0 @@
-package val
-
-import (
-	"github.com/reiver/go-erorr"
-)
-
-const (
-	errNilReceiver = erorr.Error("vals: nil receiver")
-)
---
+++ values.go
@@ -0,0 +1,91 @@
+package val
+
+// Values represents multiple values.
+//
+// Example usage:
+//
+//	var values val.Values[string]
+//	
+//	// ...
+//	
+//	values.Append("apple")
+//	values.Append("banana")
+//	values.Append("cherry")
+//	
+//	// ..
+//	
+//	last := values.Last().
+type Values[T any] struct {
+	values []T
+}
+
+func SomeValues[T any](values ...T) Values[T] {
+	return Values[T]{
+		values:values,
+	}
+}
+
+// All returns all the values as a slice.
+func (receiver Values[T]) All() []T {
+	return append([]T(nil), receiver.values...)
+}
+
+// Append appends another value to the list of values.
+func (receiver *Values[T]) Append(value T) {
+	if nil == receiver {
+		return
+	}
+
+	receiver.values = append(receiver.values, value)
+}
+
+// First returns the first value in the list of values if it exists.
+func (receiver Values[T]) First() (T, bool) {
+	var values []T = receiver.values
+
+	if len(values) <= 0 {
+		var nada T
+		return nada, false
+	}
+
+	return values[0], true
+}
+
+// FirstElse returns the first value in the list of values if it exists, else returns an alternative value.
+func (receiver Values[T]) FirstElse(alternative T) T {
+	value, something := receiver.First()
+	if !something {
+		return alternative
+	}
+
+	return value
+}
+
+// Last returns the last value in the list of values if it exists.
+func (receiver Values[T]) Last() (T, bool) {
+	var values []T = receiver.values
+
+	var length int = len(values)
+
+	if length <= 0 {
+		var nada T
+		return nada, false
+	}
+
+	return values[length-1], true
+}
+
+// LastElse returns the last value in the list of values if it exists, else returns an alternative value.
+func (receiver Values[T]) LastElse(alternative T) T {
+	value, something := receiver.Last()
+	if !something {
+		return alternative
+	}
+
+	return value
+}
+
+// Len returns the number of values in the list of values.
+func (receiver Values[T]) Len() int {
+	return len(receiver.values)
+}
